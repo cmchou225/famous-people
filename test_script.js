aa3d5d5cc223984f3390939cd0ebb384ef198e6b
Charles <cming.chou@gmail.com>
2017-04-25 12:36:41 -0700

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,23 +1,25 @@
 const pg = require('pg');
 const settings = require('./settings');
 const lastName = process.argv[2];
-const db = require('./db-query.js');
 
 const client = new pg.Client({
-  user: settings.user,
-  password: settings.password,
-  database: settings.database,
-  host: settings.hostname,
-  port: settings.port,
-  ssl: settings.ssl
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
 });
 
 client.connect((err) => {
-  if(err) {
-    return console.error('connection error', err);
+  if (err) {
+    return console.error("Connection Error", err);
   }
-  db.getPerson(lastName, (data) =>{
-    console.log('data', data, 'typeof', typeof data);
-  })
-
+  client.query("SELECT * FROM famous_people WHERE last_name = $1::text", [lastName], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0]);
+    client.end();
+  });
 });
